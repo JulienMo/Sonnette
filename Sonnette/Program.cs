46db6b3e178a9e3ab091df9db2fb0b1cdc7c1329
JulienMo <c.julien.moinard@gmail.com>
2022-03-04 11:22:45 +0100

    Gpio

--- Sonnette/Program.cs
+++ Sonnette/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Sonnette.RaspPi;
+﻿using System.Device.Gpio;
+
+namespace Sonnette.RaspPi;
 
 public class Sonnette
 {
@@ -6,6 +8,20 @@ public class Sonnette
     {
         // See https://aka.ms/new-console-template for more information
         Console.WriteLine("Hello, World!");
+
+        GpioController controller = new GpioController(PinNumberingScheme.Board);
+
+        controller.OpenPin(10, PinMode.Output);
+
+        while (true)
+        {
+            controller.Write(10, PinValue.Low);
+            Thread.Sleep(1000);
+
+            controller.Write(10, PinValue.High);
+            Thread.Sleep(1000);
+        }
+
 
     }
 }
