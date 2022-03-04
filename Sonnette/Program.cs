123fa83458a0abd9fae4e310699fc9a5a5236146
JulienMo <c.julien.moinard@gmail.com>
2022-03-04 11:04:51 +0100

    Ajoutez des fichiers projet.

---
+++ Sonnette/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Sonnette.RaspPi;
+
+public class Sonnette
+{
+    public static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+
+    }
+}
