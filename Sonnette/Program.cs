2fd89cf509089624f520c86108afea118a937cf6
Bastien <bguedon.ir2023@esaip.org>
2022-03-04 16:04:14 +0100

    Allumage led bouton  + creation ASP

--- Sonnette/Program.cs
+++ Sonnette/Program.cs
@@ -4,24 +4,34 @@ namespace Sonnette.RaspPi;
 
 public class Sonnette
 {
+
     public static void Main(string[] args)
     {
         // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World!");
 
         GpioController controller = new GpioController(PinNumberingScheme.Board);
 
+        //Déclaration des PIN
         controller.OpenPin(10, PinMode.Output);
+        controller.OpenPin(11, PinMode.Input);
+
 
         while (true)
         {
-            controller.Write(10, PinValue.Low);
-            Thread.Sleep(1000);
-
-            controller.Write(10, PinValue.High);
-            Thread.Sleep(1000);
+            SwitchLed(controller);
+            Thread.Sleep(100);
         }
+    }
 
-
+    public static void SwitchLed(GpioController controller)
+    {
+        if (controller.Read(11) == PinValue.Low)
+        {
+            controller.Write(10, PinValue.High);
+        }
+        else
+        {
+            controller.Write(10, PinValue.Low);
+        }
     }
 }
